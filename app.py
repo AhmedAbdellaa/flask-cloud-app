3c09a3679a5cc66d728decd57358e5b8d9901347
Ahmed Abdellah <62062017+AhmedAbdellaa@users.noreply.github.com>
2026-09-24 16:12:31 +0300

    Add UI branch information to welcome message

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 def hello():
     return jsonify(
         message="✨ Welcome to Cloud with abdellah ✨",
-        tip="Built with Flask, shipped by Jenkins, running in Docker."
+        tip="Built with Flask, shipped by Jenkins, running in Docker.",
+        ui="here is the ui branch "
     )
 
 if __name__ == "__main__":
